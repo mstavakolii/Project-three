7a04fdcfbd74837d0340574ae9352fafd2f60a25
mstavakolii <mstavakolii@gmail.com>
2022-01-07 14:29:12 +0330

    html css js added

---
+++ script.js
@@ -0,0 +1,11 @@
+todolist(toto(test(blah)));
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  console.log("hello");
+  if (true) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
